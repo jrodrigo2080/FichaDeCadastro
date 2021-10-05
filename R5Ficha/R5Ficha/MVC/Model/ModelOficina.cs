3005481f39c1092a9eb64ac5c17cbe074e5d27f8
José Rodrigo <jrodrigo2080@gmail.com>
2021-10-04 22:47:39 -0300

    #montagem do relatorio

---
+++ R5Ficha/R5Ficha/MVC/Model/ModelOficina.cs
@@ -0,0 +1,117 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R5Ficha.MVC.Model
+{
+    public class ModelOficina
+    {
+        public static void InserirOficina(string nome)
+        {
+            using (FbConnection con = ModelConexao.GetInstancia().GetConexao())
+            {
+                try
+                {
+                    con.Open();
+                    var sql = $"INSERT INTO OFICINA(NOME)VALUES('" + nome + "')";
+                    FbCommand comando = new FbCommand(sql, con);
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Oficina Cadastrada com Sucesso!");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Erro ao Salvar " + e.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+        }
+        public static void GetOficina(DataGridView tabela)
+        {
+            using (FbConnection con = ModelConexao.GetInstancia().GetConexao())
+            {
+                try
+                {
+                    con.Open();
+                    var sql = "select id_oficina as CODIGO, nome as NOME from OFICINA";
+                    FbCommand comando = new FbCommand(sql, con);
+                    FbDataAdapter dataAdapter = new FbDataAdapter(comando);
+                    DataTable lista = new DataTable();
+                    dataAdapter.Fill(lista);
+                    tabela.DataSource = lista;
+                }
+                catch (Exception e) { MessageBox.Show("Erro ao Listar " + e.Message); }
+                finally { con.Close(); }
+
+            }
+        }
+        public static void UpdateOficina(string nome,string codigo)
+        {
+            using (FbConnection con = ModelConexao.GetInstancia().GetConexao())
+            {
+                try
+                {
+                    if (codigo != null || codigo != "")
+                    {
+                        con.Open();
+                        var sql = $"UPDATE OFICINA SET NOME = '{nome}' WHERE ID_OFICINA = '{codigo}'";
+                        FbCommand comando = new FbCommand(sql, con);
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Informações alteradas com Sucesso!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Selecione uma oficina para deletar!");
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Erro ao Alterar " + e.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+        }
+        public static void DeleteOficina(string codigo)
+        {
+            using (FbConnection con = ModelConexao.GetInstancia().GetConexao())
+            {
+                try
+                {
+                    if (codigo != null || codigo != "")
+                    {
+                        con.Open();
+                        var sql = $"DELETE FROM OFICINA WHERE ID_OFICINA = '{codigo}'";
+                        FbCommand comando = new FbCommand(sql, con);
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Oficina deletada com sucesso!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Selecione uma oficina para deletar!");
+                    }                  
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Erro ao Alterar " + e.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
---
+++ R5Ficha/R5Ficha/Relatorio/FrmRelatorioFicha.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace R5Ficha.Relatorio
+{
+    public partial class FrmRelatorioFicha : Form
+    {
+        public FrmRelatorioFicha()
+        {
+            InitializeComponent();
+        }        
+
+        private void FrmRelatorioFicha_Load(object sender, EventArgs e)
+        {            
+            this.reportViewer1.RefreshReport();
+        }
+
+        public void GeraPDF(string inclusao,string data,string atualizacao,string dataInclusao,string nome,string oficina,string dataNasc,string sexo,string nis,
+            string naturalidade,string uf,string rg,string cpf, string emissor, string ufEmissor, string telefone,string cep, string endereco, string contatoEmergencia,
+            string foneEmergencia,string etnia,string estadoCivil, string religiao,string participaGrupo,string qualGrupo, string temFilhos, string qtsFilhos,
+            string temNetos,string qtsNetos,string indigena, string reserva, string quilombola, string comunidade, string estrangeiro, string pais)
+        {
+            inclusao = inclusao == "S" ? "(X)SIM ( )NÃO" : "( )SIM (X)NÃO";
+            atualizacao = atualizacao == "Sim" ? "(X)SIM ( )NÃO" : "( )SIM (X)NÃO";
+            participaGrupo = participaGrupo == "Sim" ? "(X)SIM ( )NÃO" : "( )SIM (X)NÃO";
+            temFilhos = temFilhos == "Sim" ? "(X)SIM ( )NÃO" : "( )SIM (X)NÃO";
+            temNetos = temNetos == "Sim" ? "(X)SIM ( )NÃO" : "( )SIM (X)NÃO";
+            sexo = sexo == "M" ? "(X)M ( )F" : "( )M (X)F";            
+            
+            //---
+            qualGrupo = qualGrupo != "" ? "Qual Grupo? " + qualGrupo : "";
+            qtsFilhos = qtsFilhos != "" ? "Quantos? " + qtsFilhos : "";
+            qtsNetos = qtsNetos != "" ? "Quantos? " + qtsNetos : "";
+            reserva = reserva != "" ? "Reserva? " + reserva : "";
+            comunidade = comunidade != "" ? "Comunidade? "+comunidade :"";
+            estrangeiro = estrangeiro != "" ? "Pais? " + pais : "";
+            //---
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("inclusao", inclusao));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("data", data));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("atualizacao", atualizacao));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("dataInclusao", dataInclusao));            
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("nome", nome));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("oficina", oficina));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("dataNascimento", dataNasc));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("sexo", sexo));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("nis", nis));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("cidade", naturalidade));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("uf", uf));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("rg", rg));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("cpf", cpf));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("emissor", emissor));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("ufEmissor", ufEmissor));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("telefone", telefone));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("cep", cep));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("endereco", endereco));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("contatoEmergencia", contatoEmergencia));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("foneEmergencia", foneEmergencia));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("etnia", etnia));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("estadoCivil", estadoCivil));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("religiao", religiao));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("participaGrupo", participaGrupo));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("qualGrupo", qualGrupo));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("filhos", temFilhos));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("qtsFilhos", qtsFilhos));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("netos", temNetos));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("qtsNetos", qtsNetos));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("indigena", indigena));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("reserva", reserva));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("quilombola", quilombola));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("comunidade", comunidade));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("estrangeiro", estrangeiro));
+            this.reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("pais", pais));
+            
+            //---
+            this.reportViewer1.RefreshReport();
+        }
+
+    }
+}
